1ca3d13ecb19df47680e32789fccae007cf198e6
Edson Maciel <edsonmaciel661@gmail.com>
2025-09-27 18:50:02 -0300

    Apaga todos os arquivos do repositório

--- itep/Pericias/db_connection.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-// db_connection.php
-$host = 'localhost'; // Defina seu host
-$db = 'Pericia';     // Nome do banco de dados
-$user = 'perito';      // Usuário do banco de dados
-$pass = 'Perito_DB_1';          // Senha do banco de dados
-?>
