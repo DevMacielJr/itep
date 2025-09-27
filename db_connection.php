b2a3d524352580de4a57f60bb1d63743b6b426f7
Edson Maciel <edsonmaciel661@gmail.com>
2025-09-27 18:53:35 -0300

    Squashed 'itep/Pericias/' content from commit 0dd70dd
    git-subtree-dir: itep/Pericias
    git-subtree-split: 0dd70dd03b9d8db92ae868eed18b8cd9deed755d

---
+++ db_connection.php
@@ -0,0 +1,8 @@
+
+<?php
+// db_connection.php
+$host = 'localhost'; // Defina seu host
+$db = 'Pericia';     // Nome do banco de dados
+$user = 'perito';      // Usuário do banco de dados
+$pass = 'Perito_DB_1';          // Senha do banco de dados
+?>
